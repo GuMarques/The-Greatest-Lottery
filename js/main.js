5be270ce850829303a41b9fbef78c5aa5482d750
Gustavo Marques <gtooliveiramarques@gmail.com>
2021-12-23 13:11:56 -0300

    feat: adição das funcionalidades dos botões Clear Game e Complete Game

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 (function(win, doc) {
+
+    var numbers = [];
+    var game;
+
+    var completeGameButton = doc.querySelector('[data-js="complete-game-button"]');
+    completeGameButton.addEventListener('click', completeGame, false);
+
+    var clearGameButton = doc.querySelector('[data-js="clear-game-button"]');
+    clearGameButton.addEventListener('click', clearGame, false);
+
     function loadGames() {
         let ajax = new XMLHttpRequest();
         ajax.open('GET', 'games.json', true);
@@ -30,8 +40,9 @@
             }
         })
     }
-    function gameChange(button, game) {
-
+    function gameChange(button, gameChoosed) {
+        game = gameChoosed;
+        numbers = [];
         var gameButtons = doc.querySelectorAll('[data-js="game-button"]');
         gameButtons.forEach((item) => {
             item.style.color = item.style.borderColor;
@@ -50,14 +61,55 @@
             boardButtons.push(doc.createElement('button'));
             boardButtons[i].innerText = i+1;
             boardButtons[i].classList = 'board-button';
-            boardButtons[i].addEventListener('click', () => boardButtonClick(game, boardButtons[i]), false);
+            boardButtons[i].dataset.js = 'board-button-' + (i+1);
+            boardButtons[i].addEventListener('click', () => boardButtonClick(boardButtons[i], i), false);
             board.appendChild(boardButtons[i]);
         }
     }
 
-    function boardButtonClick(game, button) {
-        button.style.backgroundColor = game.color;
-        console.log(game, button);
+    function boardButtonClick(button, index) {
+
+        var buttonPressed = index + 1;
+
+        if(numbers.indexOf(buttonPressed) === -1) {
+            if(numbers.length === game['max-number']) {
+                alert('O máximo de numeros para esse jogo foi atingido')
+            } else {
+                button.style.backgroundColor = game.color;
+                numbers.push(buttonPressed);
+            }
+        } else {
+            button.style.backgroundColor = '#ADC0C4';
+            numbers.splice(numbers.indexOf(buttonPressed), 1);
+        }
+
+    }
+
+    function completeGame() {
+        var numbersToComplete = game['max-number'] - numbers.length;
+
+        for(let i = 0; i < numbersToComplete; i++) {
+            var randomNumber = Math.floor((Math.random() * game.range) + 1)
+            if(numbers.indexOf(randomNumber) === -1) {
+
+                var buttonToPress = doc.querySelector(`[data-js="board-button-${randomNumber}"]`);
+                boardButtonClick(buttonToPress, randomNumber-1);
+
+            } else {
+                i--;
+            }
+        }        
+    }
+
+    function clearGame() {
+    
+        numbers.forEach((number) => {
+            var buttonToPress = doc.querySelector(`[data-js="board-button-${number}"]`)
+            buttonToPress.style.backgroundColor = '#ADC0C4';
+        })
+
+        numbers = [];
+
     }
     loadGames();
 })(window, document);
