4fb872c2a40d2b8d4daefa9576925dc8feef9b3c
Gustavo Marques <gtooliveiramarques@gmail.com>
2021-12-23 15:06:12 -0300

    feat: adição das funcionalidades dos botões Add to cart e delete from cart (icone de lata de lixo)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 (function(win, doc) {
 
     var numbers = [];
+    var cartTotalValue = 0;
     var game;
 
     var completeGameButton = doc.querySelector('[data-js="complete-game-button"]');
@@ -8,6 +9,9 @@
 
     var clearGameButton = doc.querySelector('[data-js="clear-game-button"]');
     clearGameButton.addEventListener('click', clearGame, false);
+    
+    var addGameButton = doc.querySelector('[data-js="add-game-button"]');
+    addGameButton.addEventListener('click', addGameToCart, false);
 
     function loadGames() {
         let ajax = new XMLHttpRequest();
@@ -43,6 +47,10 @@
     function gameChange(button, gameChoosed) {
         game = gameChoosed;
         numbers = [];
+
+        var gameName = doc.querySelector('[data-js="game-name"]');
+        gameName.innerText = game.type.toUpperCase();
+
         var gameButtons = doc.querySelectorAll('[data-js="game-button"]');
         gameButtons.forEach((item) => {
             item.style.color = item.style.borderColor;
@@ -110,6 +118,105 @@
 
         numbers = [];
 
+    }
+
+    function addGameToCart() {
+        if(numbers.length !== game['max-number']) {
+            alert(`Para fazer esse jogo você precisa selecionar ${game['max-number']} números.`);
+        } else {
+            var item = doc.createElement('div');
+            item.classList = 'item';
+
+            var trashCan = doc.createElement('img');
+            trashCan.alt = 'trash-icon';
+            trashCan.src = 'assets/icons/trash.svg';
+            trashCan.addEventListener('click', () => removeGame(item), false);
+
+            var bar = doc.createElement('bar');
+            bar.classList = 'bar';
+
+            var itemInfos = doc.createElement('div');
+            itemInfos.classList = 'item-infos';
+
+            var pNumbers = doc.createElement('p');
+            pNumbers.classList = 'item-numbers';
+            pNumbers.innerText = numbers.map((number, index, array) => {
+                var response = '';
+                if(index !== 0) {
+                    response += ' ';
+                }
+                if(number <= 9) {
+                    response += '0';
+                }
+                response += '' + number;
+                if(index+1 === array.length) {
+                    response += '.';
+                }
+                return response;
+            });
+
+            var itemGame = doc.createElement('div');
+            itemGame.classList = 'item-game';
+
+            var itemGameName = doc.createElement('p');
+            itemGameName.classList = 'item-game-name';
+            itemGameName.innerText = game.type;
+            itemGameName.style.color = game.color;
+
+            var itemGameValue = doc.createElement('p');
+            itemGameValue.classList = 'item-game-value';
+            const reg = /\./g;
+            var brl = new Intl.NumberFormat("pt-BR", {
+                style: 'currency',
+                currency: 'BRL'
+            }).format(game.price);
+            itemGameValue.innerText = brl;
+
+            itemGame.appendChild(itemGameName);
+            itemGame.appendChild(itemGameValue);
+            
+            itemInfos.appendChild(pNumbers);
+            itemInfos.appendChild(itemGame);
+
+            item.appendChild(trashCan);
+            item.appendChild(bar);
+            item.appendChild(itemInfos);
+
+            var cartItens = doc.querySelector('[data-js="cart-itens"]');
+            cartItens.appendChild(item);
+
+
+            cartTotalValue += game.price;
+            var totalValue = new Intl.NumberFormat("pt-BR", {
+                style: 'currency',
+                currency: 'BRL'
+            }).format(cartTotalValue);
+
+            doc.querySelector('[data-js="total-value"]').innerText = totalValue;
+
+            clearGame();
+
+        }
+
+        function removeGame(item) {
+
+            var itemValue = item.lastChild.lastChild.lastChild.innerText.replace(/^\D+/g,'');
+
+            itemValue = itemValue.replace(/,/g, '.');
+
+            cartTotalValue -= Number(itemValue);
+
+            var totalValue = new Intl.NumberFormat("pt-BR", {
+                style: 'currency',
+                currency: 'BRL'
+            }).format(cartTotalValue);
+
+            doc.querySelector('[data-js="total-value"]').innerText = totalValue;
+
+
+            var cartItens = doc.querySelector('[data-js="cart-itens"]');
+            cartItens.removeChild(item);
+        }
     }
     loadGames();
 })(window, document);
